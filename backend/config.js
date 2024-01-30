f6ffdab7d58ba09c12c46e749516cdc89211080e
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-01-31 00:42:59 +0800

    added route and add mongo

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+  'mongodb+srv://root:KlOIhEsirtsyRkAY@vide-store-mern.ojvgfr6.mongodb.net/videos-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch(() =>{
+    console.log(error)
+  });
