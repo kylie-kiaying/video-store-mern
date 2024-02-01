cbe8d5828c444f0080066367acd629fbbcd9bec1
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-02-01 14:46:36 +0800

    added route to save a new video

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,45 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Video } from "./models/videoModel.js";
 
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 app.get('/', (request, response) => {
   console.log(request);
   return response.status(234).send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+// Route for Save a new Video
+app.post('/videos', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.director ||
+      !request.body.releaseYear
+    ) {
+      return response.status(400).send({
+        message: "Required field missing",
+      });
+    }
+    const newVideo = {
+      title: request.body.title,
+      director: request.body.director,
+      releaseYear: request.body.releaseYear,
+    };
+
+    const video = await Video.create(newVideo);
+
+    return response.status(201).send(video);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({
+        message: error.message,
+      });
+  }
 });
 
 mongoose
@@ -21,6 +50,6 @@ mongoose
       console.log(`Server is listening on port ${PORT}`);
     });
   })
-  .catch(() =>{
-    console.log(error)
+  .catch((error) => {
+    console.error(error); 
   });
