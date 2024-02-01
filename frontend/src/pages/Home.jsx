bdc45ff7029255c6160d5f5f080893ac14017aab
kylie-kiaying <kylie.kiayingchua@gmail.com>
2024-02-01 18:48:25 +0800

    show videos list in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { set } from "mongoose";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/videos")
+      .then((response) => {
+        setVideos(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
 
-export default Home
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="my-8 text-3xl">Videos List</h1>
+        <Link
+          to="/videos/create"
+          className="flex items-center text-white bg-blue-500 rounded-full py-2 px-4"
+        >
+          <MdOutlineAddBox className="mr-2" />
+          Create Video
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="w-full border-separate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Director
+              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Release Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {videos.map((video, index) => (
+              <tr key={video._id} className="h-8">
+                <td className="border border-slate-700 rounded-md text-center">
+                  {index + 1}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  {video.title}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {video.director}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                  {video.releaseYear}
+                </td>
+                <td className="border border-slate-700 rounded-md text-center">
+                  <div className="flex justify-center gap-x-4">
+                    <Link to={`/videos/details/${video._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-800" />
+                    </Link>
+                    <Link to={`/videos/edit/${video._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                    </Link>
+                    <Link to={`/videos/delete/${video._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default Home;
